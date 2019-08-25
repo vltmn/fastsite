971b6782080b004ea8f117cf618271415d5a6796
vltmn <melkerveltman@gmail.com>
2019-08-25 23:23:30 +0200

    added hash comparison for s3 files to increase update speed

--- dist/s3.js
+++ dist/s3.js
@@ -15,42 +15,67 @@ const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const mime_types_1 = __importDefault(require("mime-types"));
+const crypto_1 = __importDefault(require("crypto"));
 let s3;
 exports.copyFolderToS3 = (bucketName, folder, region) => __awaiter(this, void 0, void 0, function* () {
     aws_sdk_1.default.config.update({
         region
     });
     s3 = new aws_sdk_1.default.S3();
-    yield internalCopyToS3(bucketName, folder, folder);
+    const filesToUpload = yield getFilesToUpload(bucketName, folder);
+    yield Promise.all(filesToUpload.map(ftu => putFile(ftu, bucketName)));
 });
-const internalCopyToS3 = (bucketName, folder, baseFolder) => __awaiter(this, void 0, void 0, function* () {
+// get files to upload with s3 diff
+const getFilesToUpload = (bucketName, folder) => __awaiter(this, void 0, void 0, function* () {
+    const allFiles = yield getAllFiles(folder, folder);
+    const filteredFiles = (yield Promise.all(allFiles.map(f => s3HashNotEquals(f, bucketName).then(ne => ({ f, upload: ne })))))
+        .filter(f => f.upload).map(f => f.f);
+    console.log(`Uploading ${filteredFiles.length} out of ${allFiles.length} files`);
+    return filteredFiles;
+});
+// get all recursive files from a given folder
+const getAllFiles = (folder, baseFolder) => __awaiter(this, void 0, void 0, function* () {
     const files = fs_1.default.readdirSync(folder);
-    for (const file of files) {
-        const filePath = path_1.default.join(folder, file);
-        if (fs_1.default.lstatSync(filePath).isDirectory()) {
-            // is a directory, recurse down
-            yield internalCopyToS3(bucketName, filePath, baseFolder);
-            continue;
-        }
-        const fileResp = yield getFile(filePath, baseFolder);
-        if (!fileResp) {
-            // file is not an OK mime type, continue
-            continue;
-        }
-        yield putFile(fileResp, bucketName);
+    const fileDirMap = files.map(file => path_1.default.join(folder, file))
+        .map(path => ({ path, isDir: fs_1.default.lstatSync(path).isDirectory() }));
+    const subDirFiles = yield Promise.all(fileDirMap.filter(e => e.isDir)
+        .map(p => getAllFiles(p.path, baseFolder)))
+        .then(files => files.reduce((acc, curr) => [...acc, ...curr], []));
+    const directFiles = yield Promise.all(fileDirMap.filter(e => !e.isDir)
+        .map(p => getFile(p.path, baseFolder)))
+        .then(files => files.filter(f => f !== undefined).map(f => f));
+    return [...subDirFiles, ...directFiles];
+});
+// returns true if the s3 hash is NOT equal and the file should be uploaded
+const s3HashNotEquals = (file, bucketName) => __awaiter(this, void 0, void 0, function* () {
+    const resp = yield s3.listObjectsV2({
+        Bucket: bucketName,
+        Prefix: file.s3Key
+    }).promise();
+    if (resp.$response.error || !resp.$response.data) {
+        console.error(resp.$response);
+        throw new Error('S3 list objects returned error');
     }
+    const content = resp.$response.data.Contents;
+    if (!content || content.length === 0) {
+        return true;
+    }
+    const [first, ..._] = content;
+    const s3Etag = first.ETag.replace(/^"(.*)"$/, '$1').toLocaleLowerCase();
+    const fileHash = file.hash.toLocaleLowerCase();
+    return s3Etag !== fileHash;
 });
 const getFile = (filePath, baseFolder) => __awaiter(this, void 0, void 0, function* () {
     const content = fs_1.default.readFileSync(filePath);
+    const str = content.toString('utf8');
+    const md5 = crypto_1.default.createHash('md5').update(str).digest('hex');
     const key = filePath.indexOf(baseFolder) == -1 ? filePath : filePath.substring((baseFolder + '/').length);
-    const contentType = getContentType(filePath);
-    if (!contentType) {
-        return undefined;
-    }
+    const contentType = getContentType(filePath) || 'application/octet-stream';
     return {
         s3Key: key,
         contentType,
-        data: content
+        data: content,
+        hash: md5
     };
 });
 const getContentType = (filePath) => mime_types_1.default.lookup(filePath);
--- src/s3.ts
+++ src/s3.ts
@@ -1,7 +1,8 @@
-import fs from 'fs';
+import fs, { access } from 'fs';
 import path from 'path';
 import aws, { S3 } from 'aws-sdk';
 import mime from 'mime-types';
+import crypto from 'crypto';
 
 let s3: S3;
 export const copyFolderToS3 = async (bucketName: string, folder: string, region: string) => {
@@ -9,56 +10,87 @@ export const copyFolderToS3 = async (bucketName: string, folder: string, region:
         region
     });
     s3 = new aws.S3();
-    await internalCopyToS3(bucketName, folder, folder);
+    const filesToUpload: File[] = await getFilesToUpload(bucketName, folder);
+    await Promise.all(filesToUpload.map(ftu => putFile(ftu, bucketName)));
 };
 
-const internalCopyToS3 = async (bucketName: string, folder: string, baseFolder: string) => {
-    const files = fs.readdirSync(folder);
-    for (const file of files) {
-        const filePath = path.join(folder, file);
-        if (fs.lstatSync(filePath).isDirectory()) {
-            // is a directory, recurse down
-            await internalCopyToS3(bucketName, filePath, baseFolder);
-            continue;
-        }
-        const fileResp = await getFile(filePath, baseFolder);
-        if (!fileResp) {
-            // file is not an OK mime type, continue
-            continue;
-        }
-        await putFile(fileResp, bucketName);
-    }
+// get files to upload with s3 diff
+const getFilesToUpload = async (bucketName: string, folder: string): Promise<File[]> => {
+    const allFiles = await getAllFiles(folder, folder);
+    const filteredFiles = (await Promise.all(allFiles.map(f => s3HashNotEquals(f, bucketName).then(ne => ({f, upload: ne})))))
+        .filter(f => f.upload).map(f => f.f);
+    console.log(`Uploading ${filteredFiles.length} out of ${allFiles.length} files`);
+    return filteredFiles;
 };
+
+// get all recursive files from a given folder
+const getAllFiles = async (folder: string, baseFolder: string): Promise<File[]> => {
+    const files = fs.readdirSync(folder);
+    const fileDirMap = files.map(file => path.join(folder, file))
+        .map(path => ({path, isDir: fs.lstatSync(path).isDirectory()}));
+
+    const subDirFiles: File[] = await Promise.all(fileDirMap.filter(e => e.isDir)
+        .map(p => getAllFiles(p.path, baseFolder)))
+        .then(files => files.reduce((acc, curr) => [...acc, ...curr], []));
+
+    const directFiles: File[] = await Promise.all(fileDirMap.filter(e => !e.isDir)
+        .map(p => getFile(p.path, baseFolder)))
+        .then(files => files.filter(f => f !== undefined).map(f => f as File));
+    return [...subDirFiles, ...directFiles];
+};
+
+// returns true if the s3 hash is NOT equal and the file should be uploaded
+const s3HashNotEquals = async (file: File, bucketName: string): Promise<boolean> => {
+    const resp = await s3.listObjectsV2({
+        Bucket: bucketName,
+        Prefix: file.s3Key
+    }).promise();
+    if (resp.$response.error || !resp.$response.data) {
+        console.error(resp.$response);
+        throw new Error('S3 list objects returned error');
+    }
+    const content = resp.$response.data.Contents;
+    if (!content || content.length === 0) {
+        return true;
+    }
+    const [first, ..._] = content;
+    const s3Etag = (first.ETag as string).replace(/^"(.*)"$/, '$1').toLocaleLowerCase();
+    const fileHash = file.hash.toLocaleLowerCase();
+    return s3Etag !== fileHash;
+};
+
 
 interface File {
     s3Key: string;
     contentType: string;
     data: Buffer;
+    hash: string;
 }
 
 const getFile = async (filePath: string, baseFolder: string): Promise<File | undefined> => {
     const content = fs.readFileSync(filePath);
+    const str = content.toString('utf8');
+    const md5 = crypto.createHash('md5').update(str).digest('hex');
     const key = filePath.indexOf(baseFolder) == -1 ? filePath : filePath.substring((baseFolder + '/').length);
-    const contentType = getContentType(filePath);
-    if (!contentType) {
-        return undefined;
-    }
+    const contentType = getContentType(filePath) || 'application/octet-stream';
     return {
         s3Key: key,
         contentType,
-        data: content
+        data: content,
+        hash: md5
     };
 };
 
 const getContentType = (filePath: string): string | false => mime.lookup(filePath);
 
-const putFile = async (file: File, bucket: string): Promise<void> =>
-    s3.putObject({
+const putFile = async (file: File, bucket: string): Promise<void> => {
+    return s3.putObject({
         Bucket: bucket,
         Key: file.s3Key,
         Body: file.data,
         ContentType: file.contentType
     }).promise().then(() => {return; });
+};
 
 const removeOneBatchFromBucket = async (bucket: string): Promise<number> => {
     const resp = await s3.listObjectsV2({
